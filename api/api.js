844f7717b6bcd1fbf63e7dff9320c785179dba41
Forman <forman@anyks.com>
2016-06-03 00:31:16 +0300

    Создаем систему AGL

--- api/api.js
+++ api/api.js
@@ -84,7 +84,7 @@ const anyks = require("./lib.anyks");
 			// Получаем объект запроса с геокодера
 			const init = obj => {
 				// obj.data.response.GeoObjectCollection.metaDataProperty.GeocoderResponseMetaData
-				console.log("+++++++++", obj.status, obj.data.response.GeoObjectCollection.featureMember);
+				console.log("+++++++++", obj.status, obj.data.response.GeoObjectCollection.featureMember[0].GeoObject);
 			};
 			/**
 			 * *getData Генератор для получения данных с геокодеров
