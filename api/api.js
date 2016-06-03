8c027b01bb0043350b9f8c63878ff6cecefb6f8e
Forman <forman@anyks.com>
2016-06-03 17:11:05 +0300

    Создаем систему AGL

--- api/api.js
+++ api/api.js
@@ -86,6 +86,7 @@ const anyks = require("./lib.anyks");
 				// obj.data.response.GeoObjectCollection.metaDataProperty.GeocoderResponseMetaData
 				// obj.data.response.GeoObjectCollection.featureMember[0].GeoObject
 
+				/*
 				const nobj = obj.data.response.GeoObjectCollection.featureMember[0];
 
 				const ya = {
@@ -105,6 +106,8 @@ const anyks = require("./lib.anyks");
 				};
 
 				console.log("+++++++++", obj.status, ya);
+				*/
+				console.log("+++++++++", JSON.stringify(obj));
 			};
 			/**
 			 * *getData Генератор для получения данных с геокодеров
