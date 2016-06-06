9578ef96dd40bcf9cccf4c80f5f08b570ed750c0
Forman <forman@anyks.com>
2016-06-06 15:15:47 +0300

    Создаем систему AGL

--- api/api.js
+++ api/api.js
@@ -260,7 +260,7 @@ const anyks = require("./lib.anyks");
 								|| (arr[i].contentType === 'street')
 								|| (arr[i].contentType === 'building')){
 									// Определяем дистанцию поиска
-									const distance = (arr[i].typeShort === "г" ? 100000 : 3000);
+									const distance = (arr[i].typeShort === "г" ? 150000 : 3000);
 									// Выполняем поиск ближайших станций метро
 									idObj.searchMetroFromGPS(
 										parseFloat(arr[i].lat),
