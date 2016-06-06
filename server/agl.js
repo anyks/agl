be9d8fe5f9aedbcb7d100cc675dbe2f38c4efc69
Forman <forman@anyks.com>
2016-06-06 15:12:40 +0300

    Создаем систему AGL

--- server/agl.js
+++ server/agl.js
@@ -162,7 +162,7 @@
 			// agl.getAddressFromString('Россия, Москва, Коммунарка, улица Липовый Парк');
 			
 			// agl.searchRegion("И").then(rs => console.log(rs));
-			agl.searchCity("Румянцево", "7700000000000").then(rs => console.log(rs));
+			agl.searchCity("Москва", "7700000000000").then(rs => console.log(rs));
 			// agl.searchStreet("Румянцево", "7700000000000").then(rs => console.log(rs));
 			// agl.searchHouse("12", "37019001000010900").then(rs => console.log(rs));
 		};
