8541ab1944e35b0b168a0911b3322a71538196db
Forman <forman@anyks.com>
2016-06-06 15:11:29 +0300

    Создаем систему AGL

--- api/api.js
+++ api/api.js
@@ -209,7 +209,7 @@ const anyks = require("./lib.anyks");
 						schema.update({_id: obj._id}, obj, {
 							upsert:	true,
 							multi:	true
-						}, err => if($.isset(err)) idObj.log(["update address in db", err], "error"));
+						}, err => {if($.isset(err)) idObj.log(["update address in db", err], "error");});
 					// Просто добавляем новый объект
 					} else (new schema(obj)).save();
 				});
