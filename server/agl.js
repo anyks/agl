b71a0a4291148a2e6a0c4b0e1641ef4a8280d2fc
Forman <forman@anyks.com>
2016-06-08 00:00:00 +0300

    Создаем систему AGL

--- server/agl.js
+++ server/agl.js
@@ -177,11 +177,8 @@
 						try {
 							// Формируем случайный индекс воркера
 							let index = ax.getRandomInt(0, workers.length - 1);
-
-							console.log("+++++++++", index, workers.length, JSON.parse(mess));
-
 							// Отсылаем воркеру сообщение
-							workers[index].send({message: "config", data: JSON.parse(mess)});
+							workers[index].send({action: "message", data: JSON.parse(mess)});
 						// Если возникает ошибка то выводим ее
 						} catch(e) {agl.log(['ошибка получения данных подписки из Redis', e], "error");}
 					}
