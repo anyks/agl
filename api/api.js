d7e3173c3529902fb59ccff9f282e003b501e67b
Forman <forman@anyks.com>
2016-06-16 03:38:35 +0300

    Создаем систему AGL

--- api/api.js
+++ api/api.js
@@ -4092,6 +4092,9 @@ const anyks = require("./lib.anyks");
 						const saveCache = () => {
 							// Ключ запроса
 							const key = getKeyRedisForSubject(obj);
+
+							console.log("------------", key, obj);
+
 							// Сохраняем данные в кеше
 							Agl.setRedis.call(idObj, "set", key, obj).then(callback).catch(callback);
 						};
